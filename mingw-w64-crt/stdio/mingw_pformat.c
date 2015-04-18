f0587b6b7b71dfd3b54595b7092b20d6bdf7efca
Stephen Kitt <steve@sk2.org>
2015-04-18 20:12:14 +0200

    Imported Upstream version 4.0.2

--- mingw-w64-crt/stdio/mingw_pformat.c
+++ mingw-w64-crt/stdio/mingw_pformat.c
@@ -297,7 +297,23 @@ __builtin_choose_expr (                                         \
     __builtin_choose_expr (                                     \
       __builtin_types_compatible_p (typeof (x), char),          \
         PFORMAT_LENGTH_CHAR,                                    \
-  PFORMAT_LENGTH_INT)))))
+    __builtin_choose_expr (                                     \
+      __builtin_types_compatible_p (typeof (x), __uI128),       \
+        PFORMAT_LENGTH_LLONG128,                                \
+    __builtin_choose_expr (                                              \
+      __builtin_types_compatible_p (typeof (x), unsigned long),          \
+        PFORMAT_LENGTH_LONG,                                             \
+    __builtin_choose_expr (                                              \
+      __builtin_types_compatible_p (typeof (x), unsigned long long),     \
+        PFORMAT_LENGTH_LLONG,                                            \
+    __builtin_choose_expr (                                              \
+      __builtin_types_compatible_p (typeof (x), unsigned short),         \
+        PFORMAT_LENGTH_SHORT,                                            \
+    __builtin_choose_expr (                                              \
+      __builtin_types_compatible_p (typeof (x), unsigned char),          \
+        PFORMAT_LENGTH_CHAR,                                             \
+  PFORMAT_LENGTH_INT))))))))))
+
 #else
 #define __pformat_arg_length( type )    \
   sizeof( type ) == sizeof( __tI128 )   ? PFORMAT_LENGTH_LLONG128 : \
@@ -1278,7 +1294,7 @@ void __pformat_emit_float( int sign, char *value, int len, __pformat_t *stream )
      * reserve space in the output field, for the required number of
      * decimal digits to be placed before the decimal point...
      */
-    if( stream->width > len )
+    if( stream->width >= len)
       /*
        * adjusting as appropriate, when width is sufficient...
        */
